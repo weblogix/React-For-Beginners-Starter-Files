96d3cf7fff5b8bd03313e6a7ca405b5db0872a0d
Randy Lowe <rlowe@indeed.com>
2018-05-02 18:51:15 +0900

    Lesson 4 Completed

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I'm the store picker!</p>
+        return (
+        <Fragment>
+            <h1>I'm a header</h1>
+            <form className="store-selector">
+                { console.log('i\'m Javascript') }
+                { /* This is a comment */ }
+                <h2>Please enter a store</h2>            
+                <input type="text" required placeholder="store Name" />
+                <button type="Submit"> Visit Store</button>
+            </form>            
+        </Fragment>
+        )
     }
 }
 
