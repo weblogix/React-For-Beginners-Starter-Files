433c4da65add6f35bcd7c9999ce7affab92eeb5e
Randy <randy@weblogix.ca>
2018-07-21 16:07:55 +0900

    Lesson 12 - Handling Events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,20 +1,37 @@
-import React, { Fragment } from 'react';
-
+import React, { Fragment } from "react";
+import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
-    render() {
-        return (
-        <Fragment>
-            <h1>I'm a header</h1>
-            <form className="store-selector">
-                { console.log('i\'m Javascript') }
-                { /* This is a comment */ }
-                <h2>Please enter a store</h2>            
-                <input type="text" required placeholder="store Name" />
-                <button type="Submit"> Visit Store</button>
-            </form>            
-        </Fragment>
-        )
-    }
+  myInput = React.createRef();
+  goToStore = event => {
+    // 1. stop the form from submitting
+    event.preventDefault();
+    console.log("go to store");
+    // 2. get the text from the input
+    console.log(this.myInput.value.value);
+    const storeName = this.myInput.value.value;
+    // 3. change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+  render() {
+    return (
+      <Fragment>
+        <h1>I'm a header</h1>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          {console.log("i'm Javascript")}
+          {/* This is a comment */}
+          <h2>Please enter a store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="Submit"> Visit Store</button>
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default StorePicker;
