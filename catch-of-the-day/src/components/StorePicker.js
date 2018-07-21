fc18a06d23fc5587d5724d4b6b2d430fb70f0380
Randy <randy@weblogix.ca>
2018-07-21 16:12:00 +0900

    Lesson 10 - Helper and Utility Functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,20 +1,25 @@
-import React, { Fragment } from 'react';
-
+import React, { Fragment } from "react";
+import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
-    render() {
-        return (
-        <Fragment>
-            <h1>I'm a header</h1>
-            <form className="store-selector">
-                { console.log('i\'m Javascript') }
-                { /* This is a comment */ }
-                <h2>Please enter a store</h2>            
-                <input type="text" required placeholder="store Name" />
-                <button type="Submit"> Visit Store</button>
-            </form>            
-        </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <h1>I'm a header</h1>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          {console.log("i'm Javascript")}
+          {/* This is a comment */}
+          <h2>Please enter a store</h2>
+          <input
+            type="text"
+            required
+            placeholder="store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="Submit"> Visit Store</button>
+        </form>
+      </Fragment>
+    );
+  }
 }
 
 export default StorePicker;
