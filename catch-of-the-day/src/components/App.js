5f40be19b897834018be722c72e23e2f0fe6454f
Randy Lowe <rlowe@indeed.com>
2019-01-20 19:10:08 +0900

    Lesson 15 complete

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -21,10 +21,20 @@ class App extends React.Component {
       fishes
     });
   };
+  
   loadSamplesFishes = () => {
-    this.setState({
+    this.setState({ 
       fishes: sampleFishes
     })
+  }
+
+  addToOrder = (key) => {
+    // 1. Take a copy of state
+    const order = {...this.state.order}
+    // 2. Either add to the order. or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order })
   }
   render() {
     return (
@@ -33,7 +43,11 @@ class App extends React.Component {
           <Header tagline="Randy is awesome" age={1} />
           <ul className="fishes">
           {Object.keys(this.state.fishes).map(key => (
-            <Fish key={key} details={this.state.fishes[key]}/>
+            <Fish 
+            key={key} 
+            index={key}
+            details={this.state.fishes[key]} 
+            addToOrder={this.addToOrder}/>
           ))}
           </ul>
         </div>
